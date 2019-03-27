97d98ac587908590a454efdfcfc766ff67eee03c
netb258 <netb258@abv.bg>
2019-03-27 14:18:34 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject chip8 "0.1.0-SNAPSHOT"
   :description "Chip8 emulator written in Clojure. Uses Quil for rendering."
   :url "http://example.com/FIXME"
-  :plugins [[cider/cider-nrepl "0.14.0"]]
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
